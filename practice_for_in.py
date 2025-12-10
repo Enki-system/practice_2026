f1c49a75674f3cc0d508fe0725095467436daf42
Enki-system <syuukatu0713@gmail.com>
2025-12-10 18:37:51 +0900

    Base_20251210

--- practice_for_in.py
+++ practice_for_in.py
@@ -5,3 +5,23 @@ member_data = {
     "Cさん": 50,
     "Dさん": 8
 }
+
+#1. for文でデータ一つずつ確認しながら、特定の条件を判定する
+print("参加資格チェック")
+ADULT_AGE = 18
+
+for name, age in member_data.items():
+
+    if age >=ADULT_AGE:
+        print(f"{name}({age}歳):参加資格あり (大人)")
+    else:
+        print(f"{name}({age}歳):参加資格なし (子供)")
+
+# 2. 条件を満たす要素を数える
+adult_count = 0
+for age in member_data.values():
+
+    if age >= ADULT_AGE:
+        adult_count += 1
+
+print(f"\n合計で、大人の参加資格を持つメンバーは{adult_count}人です。")
